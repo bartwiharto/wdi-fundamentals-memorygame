364cf8e54ab4c273b0a747525f7be9b9005f723e
Bart Wiharto <bartwiharto@Barts-MacBook-Air.local>
2018-02-23 20:47:43 -0700

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("Userflipped" + cardOne);
+console.log("Userflipped" + cardThree);
