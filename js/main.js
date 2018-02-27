ea56b091b3f5083f4458e599d070b27d1b5c8eb7
Bart Wiharto <bartwiharto@Barts-MacBook-Air.local>
2018-02-27 15:12:48 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-console.log("Userflipped" + cardOne);
-console.log("Userflipped" + cardThree);
+cardsInPlay.push(cardOne)
+	console.log("User flipped queen");
+
+cardsInPlay.push(cardTwo)
+	console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");		
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+
